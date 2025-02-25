98e16a8e460d6bd1cd6cd1c6f95a8c3116350cfa
Clement <clement@Clements-MacBook-Pro.local>
2025-02-25 01:08:36 +0100

    features/front - add login + register pages

--- front/src/app/pages/home/home.component.ts
+++ front/src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  constructor() {}
+  constructor(private router: Router) {}
 
   ngOnInit(): void {}
+
+  linkedToLogin(){
+    this.router.navigate(['/login']);
+  }
+
+  linkedToRegister(){
+    this.router.navigate(['/register']);
+  }
 
   start() {
     alert('Commencez par lire le README et à vous de jouer !');
